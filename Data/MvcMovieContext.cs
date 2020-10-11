bf759c4eebcb4b05ea04303a9c92be236b9fd041
clucht <c.lucht@icloud.com>
2020-10-11 17:40:04 -0500

    add model/movies controller

---
+++ Data/MvcMovieContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MvcMovie.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options) : base(options) { }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
